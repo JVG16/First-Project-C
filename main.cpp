600b9193ae9e7b5272d8d423b47b71be67f24e11
JVG16 <jimenavg0416@gmail.com>
2025-05-27 11:59:07 -0600

    Create a menu

--- main.cpp
+++ main.cpp
@@ -1,9 +1,30 @@
+// Universidad Estatal a Distancia.
+// Estudiante: Jimena Velásquez Gómez.
+// Asignatura: Introducción a la Programación.
+// Proyecto #1.
+// Temas 1 y 2.
+
+
 #include <iostream>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!, I like to write Hello" << endl;
+    char opcion=0;
+
+    cout << "Menu Principal"<< endl;
+    cout << "1.Buscar Tarea"<< endl;
+    cout << "2.Mostrar todas las tareas" << endl;
+    cout << "3.Mostrar todas las tareas" << endl;
+    cout << "4.Eliminar tareas" << endl;
+    cout << "5.Salir del programa" << endl;
+
+    cout << "Por favor digite un numero del 1 al 5" << endl;
+    cin >> opcion;
+
     return 0;
-}
+     }
+
+
+
