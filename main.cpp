8ed1570da5349a2a6b42b2ce6eda4729521fc659
JVG16 <jimenavg0416@gmail.com>
2025-05-30 11:30:37 -0600

    Finalizado case 2 (#3)

--- main.cpp
+++ main.cpp
@@ -15,6 +15,7 @@ int main()
     int option;
     do
     {
+
         cout << "Menu Principal"<< endl;
         cout << "1.Agregar Tareas"<< endl;
         cout << "2.Buscar Tarea" << endl;
@@ -24,28 +25,68 @@ int main()
 
         cout << "Por favor digite un numero del 1 al 5" << endl;
         cin >> option;
-        // TODO proteger option cuando se ingresa una letra
 
-        // valores hardcoded para hacer puntos 2 y 3
-        string matriz[3][4] =
+            string matriz[3][4] =
         {
             {"1234", "Clasificar material", "20/02/2025", "35"},
             {"6543", "Elaborar ensayo", "21/02/2025", "36"},
             {"5555", "Presentar ensayo", "22/02/2025", "37"}
+
         };
 
         switch(option)
         {
         case 1:
         {
-            // nada
+             string matriz[3][4] =
+        {
+            {"1234", "Clasificar material", "20/02/2025", "35"},
+            {"6543", "Elaborar ensayo", "21/02/2025", "36"},
+            {"5555", "Presentar ensayo", "22/02/2025", "37"}
+
+        };
             break;
         }
         case 2:
-            cout << "\nHas seleccionado la Opcion 2\n";
-            cout << matriz[0][0] << endl;
-            // Aquí iría el código para la opción 2
-            break;
+        {
+            // Buscar código.
+
+        string Buscar;
+        bool Encontrada = false ;
+
+        cout << "Digite el código de la tarea que desea buscar." << endl;
+        cin >> Buscar;
+
+        for (int i = 0; i < 3; i++)
+        {
+            if (matriz[i][0]==Buscar)
+            {
+                Encontrada = true;
+
+                cout << "Tarea encontrada" << endl;
+
+                cout << "+--------------+----------------+-----------------------+-----------------------" << endl;
+
+                cout << "     Código         Nombre           Fecha Vencimiento     Minutos Invertidos" << endl;
+
+                cout << "+--------------+----------+-----------------------------+-----------------------" << endl;
+
+
+                cout << "     " << matriz[i][0] ;
+                cout << "        " << matriz[i][1] ;
+                cout << "       " << matriz[i][2] ;
+                cout << "        " << matriz[i][3] << " " ;
+                break;
+            }
+        }
+
+        if ( !Encontrada)
+        {
+            cout << "Tarea no encontrada";
+        }
+break;
+ }
+
 
         case 3:
             cout << "\nHas seleccionado la Opcion 3\n";
@@ -53,8 +94,6 @@ int main()
             break;
 
         case 4:
-            cout << "\nHas seleccionado la Opcion 4\n";
-            // Aquí iría el código para la opción 4
             break;
 
         case 5:
@@ -63,11 +102,11 @@ int main()
 
         default:
             cout << "Opción no válida" << endl;
+             }
+
         }
+        while(option != 5);   // Repetir hasta que elija salir (opción 5)
 
+        return 0 ;
     }
-    while(option != 5);   // Repetir hasta que elija salir (opción 5)
-
-    return 0;
-}
 
