cdc62e0cafd6f71f05f1c38ce3eb1b3d6d753868
JVG16 <jimenavg0416@gmail.com>
2025-06-06 10:09:22 -0600

    listado de tareas (#4)

--- main.cpp
+++ main.cpp
@@ -26,72 +26,104 @@ int main()
         cout << "Por favor digite un numero del 1 al 5" << endl;
         cin >> option;
 
-            string matriz[3][4] =
+        string matriz[5][4] =
         {
             {"1234", "Clasificar material", "20/02/2025", "35"},
             {"6543", "Elaborar ensayo", "21/02/2025", "36"},
-            {"5555", "Presentar ensayo", "22/02/2025", "37"}
-
+            {"5555", "Presentar ensayo", "22/02/2025", "37"},
+            {"", "", "", ""},
+            {"", "", "", ""}
         };
 
         switch(option)
         {
         case 1:
         {
-             string matriz[3][4] =
-        {
-            {"1234", "Clasificar material", "20/02/2025", "35"},
-            {"6543", "Elaborar ensayo", "21/02/2025", "36"},
-            {"5555", "Presentar ensayo", "22/02/2025", "37"}
 
-        };
             break;
         }
         case 2:
         {
             // Buscar código.
 
-        string Buscar;
-        bool Encontrada = false ;
+            string Buscar;
+            bool Encontrada = false ;
 
-        cout << "Digite el código de la tarea que desea buscar." << endl;
-        cin >> Buscar;
+            cout << "Digite el código de la tarea que desea buscar." << endl;
+            cin >> Buscar;
 
-        for (int i = 0; i < 3; i++)
-        {
-            if (matriz[i][0]==Buscar)
+            for (int i = 0; i < 5; i++)
             {
-                Encontrada = true;
+                if (matriz[i][0]==Buscar)
+                {
+                    Encontrada = true;
 
-                cout << "Tarea encontrada" << endl;
+                    cout << "Tarea encontrada" << endl;
 
-                cout << "+--------------+----------------+-----------------------+-----------------------" << endl;
+                    cout << "+--------------+----------------+-----------------------+-----------------------" << endl;
+
+                    cout << "     Código         Nombre           Fecha Vencimiento     Minutos Invertidos" << endl;
+
+                    cout << "+--------------+----------+-----------------------------+-----------------------" << endl;
+
+
+                    cout << "     " << matriz[i][0] ;
+                    cout << "        " << matriz[i][1] ;
+                    cout << "       " << matriz[i][2] ;
+                    cout << "        " << matriz[i][3] << " " ;
+                    break;
+                }
+            }
+
+            if ( !Encontrada)
+            {
+                cout << "Tarea no encontrada";
+            }
+            break;
+        }
+
+
+        case 3:
+        {
+            bool MostrarTareas= false;
+            for (int i=0; i<5; i++)
+            {
+                if(matriz[i][0] != "")
+                {
+                    MostrarTareas= true;
+                    break;
+                }
+            }
+
+            if (!MostrarTareas)
+            {
+                cout << " No hay tareas registradas " << endl;
+            }
+
+            else
+            {
+                cout << " Mostrar tareas registradas " << endl;
+
+                cout << "+--------------+----------+-----------------------------+-----------------------" << endl;
 
                 cout << "     Código         Nombre           Fecha Vencimiento     Minutos Invertidos" << endl;
 
                 cout << "+--------------+----------+-----------------------------+-----------------------" << endl;
 
-
-                cout << "     " << matriz[i][0] ;
-                cout << "        " << matriz[i][1] ;
-                cout << "       " << matriz[i][2] ;
-                cout << "        " << matriz[i][3] << " " ;
-                break;
+                for (int i = 0; i < 5; i++)
+                {
+                    if(matriz[i][0] != "")
+                    {
+                        cout << "    " << matriz[i][0];
+                        cout << "    " << matriz[i][1];
+                        cout << "    " << matriz[i][2];
+                        cout << "    " << matriz[i][3] << endl;
+                    }
+                }
             }
-        }
 
-        if ( !Encontrada)
-        {
-            cout << "Tarea no encontrada";
-        }
-break;
- }
-
-
-        case 3:
-            cout << "\nHas seleccionado la Opcion 3\n";
-            // Aquí iría el código para la opción 3
             break;
+        }
 
         case 4:
             break;
@@ -102,11 +134,11 @@ break;
 
         default:
             cout << "Opción no válida" << endl;
-             }
-
         }
-        while(option != 5);   // Repetir hasta que elija salir (opción 5)
 
-        return 0 ;
     }
+    while(option != 5);   // Repetir hasta que elija salir (opción 5)
+
+    return 0 ;
+}
 
